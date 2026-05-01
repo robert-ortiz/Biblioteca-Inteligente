a53cfb331b7fdba275f632addb281b004542485e
KathM00 <kathymontamejia@gmail.com>
2026-04-30 21:56:31 -0400

    feat: agregando constantes y states

--- src/app/buscar/page.tsx
+++ src/app/buscar/page.tsx
@@ -1,7 +1,16 @@
-// src/app/buscar/page.tsx
 "use client";
 import { useState } from "react";
 import { searchBooks } from "@/services/openLibraryService";
 import BookCard from "@/components/BookCard";
 import Loading from "@/components/Loading";
 import ErrorMessage from "@/components/ErrorMessage";
+
+export default function BuscarPage() {
+  const [query, setQuery] = useState("");
+  const [type, setType] = useState("q"); 
+  const [sort, setSort] = useState(""); 
+  const [results, setResults] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [hasSearched, setHasSearched] = useState(false); 
+}
